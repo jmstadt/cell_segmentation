daae488fb797b3eb73cf956d3c8e203358e4c569
jmstadt <jmstadt@users.noreply.github.com>
2019-05-06 15:02:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, request
-from fastai.text import *
-import requests
-import os.path
+from fastai.vision import *
+from fastai.metrics import error_rate
+from flask import Flask, request, url_for, flash
+from werkzeug import secure_filename
+from flask import send_from_directory
+
+import numpy as np
+import os
+from os import rename, listdir
+from PIL import Image
+
+class SegLabelListCustom(SegmentationLabelList):
+    def open(self, fn): return open_mask(fn, div=True)
+
+class SegItemListCustom(SegmentationItemList):
+    _label_cls = SegLabelListCustom
 
 path = ''
-export_file_url = 'https://www.dropbox.com/s/awbay851mj1sgj1/movies_export.pkl?dl=1'
-export_file_name = 'movies_export.pkl'
+export_file_url = 'https://www.dropbox.com/s/bjszupvu7a15ccb/cell_export.pkl?dl=1'
+export_file_name = 'cell_export.pkl'
 
 def down_load_file(filename, url):
     """
@@ -36,19 +48,67 @@ download_if_not_exists(export_file_name, export_file_url)
 
 learn = load_learner(path, export_file_name)
 
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = set(['jpg', 'png'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':  #this block is only entered when the form is submitted
-        review_text = request.form.get('review_text')
-        preds = learn.predict(review_text)
+def upload_file():
 
-        return '''<h1>The review text is: {}</h1><h1>The prediction value is: {}</h1>'''.format(review_text, preds)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
 
-    return '''<form method="POST">
-                  Review_text: <input type="text" name="review_text"><br>
-                  <input type="submit" value="Submit"><br>
-              </form>'''
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            #filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            image = open_image(filename)
+            image_url = url_for('uploaded_file', filename=filename)
+            think = learn.predict(image)
+            think_np = np.array(think[1])
+            think_np.shape = (256,256)
+            think_np = think_np.astype(int)
+            think_np[think_np > 0] = 255
+            think_im = Image.fromarray((think_np).astype('uint8'), mode='L')
+            think_im.save(os.path.join(app.config['UPLOAD_FOLDER'], 'think2_im.png'))
+            think_im_url = url_for('uploaded_file', filename='think2_im.png')
+            print(think_im_url)
+            #image.show(y=learn.predict(image)[0])
+            return '''<h1>The cell image is:</h1>
+            <img src= "{}" height = "85" width="200"/>
+            <h1>The cell segmentation is:</h1>
+            <img src= "{}" height = "85" width="200"/>'''.format(image_url, think_im_url)
 
 
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload an image of Cells</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
